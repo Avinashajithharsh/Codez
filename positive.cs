bb230f12b08483b675b5ae0ea964624863db5ad1
Avinashajithharsh <30927229+Avinashajithharsh@users.noreply.github.com>
2017-08-11 16:50:07 +0530

    Create positive.cs

---
+++ positive.cs
@@ -0,0 +1,17 @@
+using name space
+public class test 
+{
+pubilc static void main()
+{
+int number;
+    if (number <= 0.0)
+    {
+        if (number == 0.0)
+            Console.Write("You entered 0.");
+        else
+            Console.Write("You entered a negative number.");
+    }
+    else
+        Console.Write("You entered a positive number.");
+    return 0;
+}
