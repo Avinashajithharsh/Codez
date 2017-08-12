a3e1eff86f7e917da7ae3fcf9c49c2ee5cdc288d
Avinashajithharsh <30927229+Avinashajithharsh@users.noreply.github.com>
2017-08-12 16:54:12 +0530

    Create alha.cs

---
+++ alha.cs
@@ -0,0 +1,17 @@
+
+using name space
+public class test 
+{
+pubilc static void main()
+{ 
+int double n1,n2, n3;
+Console.Write("n1,n2,n3"0;
+
+ if(n1>=n2 &&n1>=n3)
+Console.wtrite("enter the value of n1,n2,n3");
+if(n2>=n1 && n3>=n2)
+Console.Writeline("enter the value of n1,n2n,n3");
+if(n3>=n2 &&n2>=n1)
+Console.Write("enter the value of n1,n2,n3");
+}
+}
